7491b4ff5db326cefe78792a558e1ff8f21bf4f1
IvandeCoelho <ivandealveslp@gmail.com>
2025-03-13 19:51:24 -0300

    updates
    update profile.php

--- profile-farm.php
+++ profile-farm.php
@@ -2,11 +2,8 @@
 
 $id = mysqli_real_escape_string($conn, $_GET['id-farm']);
 
-//$sqlExe = mysqli_query($conn, "SELECT * FROM produto WHERE id = $id");
-
 $sqlExe = mysqli_query($conn, "SELECT * FROM farmacia WHERE id = $id");
-
-while ($dado = mysqli_fetch_assoc($sqlExe)) {
+while ($dados_farmacia = mysqli_fetch_assoc($sqlExe)) {
     ?>
 
     <div class="container">
@@ -14,14 +11,19 @@ while ($dado = mysqli_fetch_assoc($sqlExe)) {
         <div class="profile-container">
             <div class="profile">
                 <div class="profile-info">
-                    <img src="./includes/<?php echo $dado['imagem_perfil'] ?>" alt="kkkkkkkk" class="profile-logo">
+                    <img src="./includes/<?php echo $dados_farmacia['imagem_perfil'] ?>" alt="kkkkkkkk"
+                        class="profile-logo">
                     <div>
-                        <h1>Farmácia</h1>
-                        <span><i class="bi bi-star-fill"></i></span>
-                        <span><i class="bi bi-star-fill"></i></span>
-                        <span><i class="bi bi-star-fill"></i></span>
-                        <span><i class="bi bi-star-fill"></i></span>
-                        <span><i class="bi bi-star-fill"></i></span>
+                        <h1><?php echo $dados_farmacia['nome'] ?></h1>
+                        <span class="badge text-bg-warning">
+                            <?php echo $dados_farmacia['avaliacao'] ?>
+                        </span>
+                        <?php
+                        for ($i = 1; $i <= round($dados_farmacia['avaliacao']); $i++) {
+                            echo '<span><i class="bi bi-star-fill"></i> </span>';
+                        }
+                        ?>
+                        </span>
                     </div>
                 </div>
                 <div class="profile-details">
@@ -30,6 +32,7 @@ while ($dado = mysqli_fetch_assoc($sqlExe)) {
                 </div>
             </div>
 
+            <!-- colocar boxshadow -->
             <div class="search-bar-low">
                 <div class="search-bar-div">
                     <span><i class="bi bi-search"></i></span>
@@ -41,31 +44,39 @@ while ($dado = mysqli_fetch_assoc($sqlExe)) {
             <section class="related-products">
                 <h4> <span><i class="bi bi-basket2-fill"></i></span> Disponível no estoque</h4>
 
-                <div class="products">
+                <div class="products" style="border: 1px solid green;">
+                    <?php
 
-                    <div class="product-card">
-                        <div class="product-header">
-                            <img src="./includes/<?php echo $dado['imagem_perfil'] ?>" alt="Logo da Drogaria" class="logo">
-                            <span class="pharmacy-name">Drogaria Loiola</span>
-                            <span class="favorite"><i class="fa-regular fa-heart"></i></span>
-                        </div>
-                        <div class="product-card-image">
-                            <a href="./review/product-review.html">
-                                <img src="../img/generico.png" alt="Medicamento">
-                            </a>
-                        </div>
-                        <div class="details">
-                            <p class="product-name">Medicamento genérico com um nome muito
-                                grandeKAKAKAKAKAKAAKAKAKAKAKAKAKAKAL</p>
-                            <p class="price">R$ 69,24</p>
-                            <div class="add-to-cart">
-                                <button class="add-to-cart-btn"
-                                    onclick="addToCart('Weiva', 24.69, 'img/generico.png', 'Drogaria Loiola')">
-                                    <i class="bi bi-bag-check"></i> ADICIONAR
-                                </button>
+                    $sqlProd = mysqli_query($conn, "SELECT p.id, p.nome, p.preco_unitario, p.descricao, p.caminho_galeria, f.imagem_perfil, f.avaliacao FROM produto AS p INNER JOIN farmacia AS f ON p.fk_farmacia_id = f.id INNER JOIN categoria AS c ON  p.fk_categoria_id = c.id WHERE p.fk_farmacia_id = $id;");
+
+                    while ($dados_prod = mysqli_fetch_assoc($sqlProd)) {
+                        ?>
+                        <div class="product-card" style="border: 1px solid red;">
+                            <div class="product-header">
+                                <img src="./includes/<?php echo $dados_farmacia['imagem_perfil'] ?>" alt="Logo da Drogaria"
+                                    class="logo">
+                                <span class="pharmacy-name"><?php echo $dados_farmacia['nome'] ?></span>
+                                <span class="favorite"><i class="fa-regular fa-heart"></i></span>
+                            </div>
+                            <div class="product-card-image">
+                                <a href="./review/product-review.html">
+                                    <img src="./<?php echo $dados_prod['caminho_galeria'] ?>" alt="Medicamento">
+                                </a>
+                            </div>
+                            <div class="details">
+                                <p class="product-name"><?php echo $dados_prod['nome'] ?></p>
+                                <span><?php echo $dados_prod['descricao'] ?></span>
+                                <p class="price">R$ <?php echo $dados_prod['preco_unitario'] ?></p>
+                                <div class="add-to-cart">
+                                    <button class="add-to-cart-btn"
+                                        onclick="addToCart('Weiva', 24.69, 'img/generico.png', 'Drogaria Loiola')">
+                                        <i class="bi bi-bag-check"></i> ADICIONAR
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                    </div>
+
+                    <?php } ?>
 
                 </div>
         </div>
